f845acf6e98e2b0f658a3e75fd549a8a5634a5ff
romantihiy <romantihomirov1@gmail.com>
2022-04-25 03:16:10 +0300

    Parser refactoring

--- main.py
+++ main.py
@@ -4,57 +4,44 @@ import datetime
 import requests
 import time
 import os
+import traceback
 
-def numeralparse(text):
-    with open('numerals.json') as f:
-        numerals = json.load(f)
-    for key in list(numerals.keys())[::-1]:
-        text = text.replace(key, numerals[key])
-    return text
-
-def parse(text):
+def parse(tokens):
     analyzer = pymorphy2.MorphAnalyzer()
-    text = text.replace('-', ' ')
-    text = numeralparse(text)
-    for pretext in ['от', 'с', 'из', 'до', 'на', 'к', 'в']:
-        text = text.replace(pretext + ' ', '')
-    text = text.split(' ')
-    for index, part in enumerate(text):
-        if part.isdigit():
-            continue
-        text[index] = analyzer.parse(part)[0].inflect({'nomn'}).word
-    text = ' '.join(text)
-    text = text.replace('километр', 'км')
+    stopwords = ['от', 'с', 'из', 'до', 'на', 'к', 'в']
+    tokens = list(filter(lambda x: x and x not in stopwords, tokens.copy()))
+    for index, token in enumerate(tokens):
+        nomn = analyzer.parse(token)[0].inflect({'nomn'})
+        if nomn:
+            tokens[index] = nomn.word
+        if tokens[index] == 'километр':
+            tokens[index] = 'км'
+    text = ' '.join(tokens)
     return text
 
-def getdate(date):
-    now = datetime.datetime.now()
-    year = now.year
-    if 'year' in date:
-        year = date['year'] if not date['year_is_relative'] else date['year'] + year
+def getdate(yandex_date):
+    date = datetime.datetime.now()
     
-    month = now.month
-    if 'month' in date:
-        month = date['month'] if not date['month_is_relative'] else date['month'] + month
-
-    day = now.day
-    if 'day' in date:
-        day = date['day'] if not date['day_is_relative'] else date['day'] + day
-    
-    hour = now.hour
-    if 'hour' in date:
-        hour = date['hour'] if not date['hour_is_relative'] else date['hour'] + hour
-    
-    minute = now.minute
-    if 'minute' in date:
-        minute = date['minute'] if not date['minute_is_relative'] else date['minute'] + minute
-    
-    return datetime.datetime(year, month, day, hour, minute)
+    units = [
+        {'unit' : 'year', 'minutes': 525600, 'argument': {'year': yandex_date.get('year')}},
+        {'unit' : 'month', 'minutes': 43200, 'argument': {'month': yandex_date.get('month')}},
+        {'unit' : 'day', 'minutes': 1440, 'argument': {'day': yandex_date.get('day')}},
+        {'unit' : 'hour', 'minutes': 3600, 'argument': {'hour': yandex_date.get('hour')}},
+        {'unit' : 'minute', 'minutes': 1, 'argument': {'minute': yandex_date.get('minute')}}
+    ]
+    for unit in units:
+        if unit['unit'] in yandex_date:
+            if yandex_date[unit['unit'] + '_is_relative']:
+                date += datetime.timedelta(minutes=unit['minutes']*yandex_date[unit['unit']])
+            else:
+                date = date.replace(**unit['argument'])
+    return date
 
 def getticket(departure, arrival, date, apikey):
     request = requests.get('https://api.rasp.yandex.net/v3.0/search/', 
         params={'apikey': apikey, 'from': departure, 'to': arrival, 'lang': 'ru_RU', 
             'date': date.strftime('%Y-%m-%d')})
+
     if not request.ok:
         return None
     
@@ -80,17 +67,31 @@ def addnull(integer):
     else:
         return str(integer)
 
-def engine(intents):
+def engine(tokens, entities, intents):
     helptext = '''Скажи мне станцию отправления, станцию назначения и время. ''' +\
     '''Например, "Едем с Ильинской на Фабричную завтра в 9 утра"'''
     dontunderstand = 'Прости, но я не поняла твою команду. Повтори еще раз или скажи "помогите"'
+    
+    os.environ['TZ'] = 'Europe/Moscow'
+    time.tzset()
+
     if 'YANDEX.HELP' in intents:
         return {'text': helptext, 'end_session': False}
     if 'mainintent' not in intents:
         return {'text': dontunderstand, 'end_session': False}
     
-    departure = parse(intents['mainintent']['slots']['from']['value'])
-    arrival = parse(intents['mainintent']['slots']['to']['value'])
+    for entity in entities:
+        if entity['type'] == 'YANDEX.NUMBER':
+            tokens[entity['tokens']['start']] = str(entity['value'])
+            for index in range(entity['tokens']['start'] + 1, entity['tokens']['end']):
+                tokens[index] = ''
+
+    slots = intents['mainintent']['slots']
+
+    departure = parse(tokens[ 
+        slots['from']['tokens']['start'] : slots['from']['tokens']['end']])
+    arrival = parse(tokens[ 
+        slots['to']['tokens']['start'] : slots['to']['tokens']['end']])
 
     with open('moscow_region.json') as f:
         stationcodes = json.load(f)
@@ -102,12 +103,10 @@ def engine(intents):
     if not arrivalcode:
         return {'text': f'''Прости, но я не знаю станцию "{arrival}"''', 'end_session': False}
     
-    os.environ['TZ'] = 'Europe/Moscow'
-    time.tzset()
     date = datetime.datetime.now()
-    if 'when' in intents['mainintent']['slots']:
-        date = getdate(intents['mainintent']['slots']['when']['value'])
-    
+    if 'when' in slots:
+        date = getdate(slots['when']['value'])
+
     with open('apikey.txt') as f:
         apikey = f.read()
 
@@ -136,9 +135,13 @@ def handler(event, context):
     if 'request' in event and 'original_utterance' in event['request'] \
                 and len(event['request']['original_utterance']) > 0:
         try:
-            response = engine(event['request']['nlu']['intents'])
+            response = engine(event['request']['nlu']['tokens'],
+                event['request']['nlu']['entities'], 
+                    event['request']['nlu']['intents'])
         except Exception as e:
             response = {'text': e, 'end_session': False}
+            # response = {'text': traceback.format_tb(e.__traceback__), 'end_session': False}
+            # response = {'text': 'Ой, кажется я сломалась', 'end_session': True}
     return {
         'version': event['version'],
         'session': event['session'],
